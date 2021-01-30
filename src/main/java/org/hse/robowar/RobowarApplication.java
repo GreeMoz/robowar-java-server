056462a0e7d08254b5c805fd16dc2097cbbb3644
v.krasnov <v.krasnov@lunawealth.com>
2021-01-30 20:00:28 +0300

    minor

--- src/main/java/org/hse/robowar/RobowarApplication.java
+++ src/main/java/org/hse/robowar/RobowarApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RobowarApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RobowarApplication.class, args);
     }
