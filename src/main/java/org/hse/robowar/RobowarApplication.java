5240031ec13d3b1751df09d40b35e699d5b6e176
v.krasnov <v.krasnov@lunawealth.com>
2021-01-30 17:23:38 +0300

    init

---
+++ src/main/java/org/hse/robowar/RobowarApplication.java
@@ -0,0 +1,12 @@
+package org.hse.robowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobowarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobowarApplication.class, args);
+    }
+}
