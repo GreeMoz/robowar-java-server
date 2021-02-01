a37f9c6f5435ec9e65d4000319e3f1076fd66dea
v.krasnov <v.krasnov@lunawealth.com>
2021-02-02 02:25:44 +0300

    encrypted pass

--- src/main/resources/script/add_account_and_roles.sql
+++ src/main/resources/script/add_account_and_roles.sql
@@ -9,7 +9,7 @@ BEGIN
 		(gen_random_uuid(), 'User', 'USER');
 
 	insert into account(id, login, password, status) values
-		(admin_id, 'admin', 'admin', 'ACTIVE');
+		(admin_id, 'admin', '$2a$04$fiLDu.QcPKVO.x9RqjrYK.D5Ek7WMatwF0ATlNvwpk9Rs6rvxe8BO', 'ACTIVE');
 
 	insert into account_roles(account_id, role_id) values
 		(admin_id, admin_role_id);
