893bcb85070304513d62540df26e0d538a34f796
v.krasnov <v.krasnov@lunawealth.com>
2021-02-05 20:39:27 +0300

    add annotations

--- src/main/java/org/hse/robowar/dto/AccountDTO.java
+++ src/main/java/org/hse/robowar/dto/AccountDTO.java
@@ -1,5 +1,8 @@
 package org.hse.robowar.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hse.robowar.model.Role;
 import org.hse.robowar.model.Status;
 
@@ -7,6 +10,9 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDTO implements Serializable {
 
     private UUID id;
