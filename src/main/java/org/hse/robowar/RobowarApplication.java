79dcb5299f0813952761b3af92f4933f6f234b2f
v.krasnov <v.krasnov@lunawealth.com>
2021-02-06 11:01:59 +0300

    scan entity

--- src/main/java/org/hse/robowar/RobowarApplication.java
+++ src/main/java/org/hse/robowar/RobowarApplication.java
@@ -2,7 +2,9 @@ package org.hse.robowar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("org.hse.robowar.model")
 @SpringBootApplication
 public class RobowarApplication {
     public static void main(String[] args) {
