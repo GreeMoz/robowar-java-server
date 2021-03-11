28efed66e6939b476217fa028f46caa401560bce
v.krasnov <v.krasnov@lunawealth.com>
2021-03-11 16:14:12 +0300

    add controller and refactor

---
+++ src/main/java/org/hse/robowar/controller/LeagueController.java
@@ -0,0 +1,9 @@
+package org.hse.robowar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/league")
+public class LeagueController {
+}
