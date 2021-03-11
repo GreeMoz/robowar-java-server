13095162145e9b783ed00d8005758580b76f13f8
Алексей <alexche3645@mail.ru>
2021-03-11 09:12:39 +0300

    Updated sql code for adding base users

--- src/main/resources/script/add_account_and_roles.sql
+++ src/main/resources/script/add_account_and_roles.sql
@@ -8,8 +8,8 @@ BEGIN
 		(admin_role_id, 'Admin', 'ADMIN'),
 		(gen_random_uuid(), 'User', 'USER');
 
-	insert into account(id, login, password, status) values
-		(admin_id, 'admin', '$2a$04$fiLDu.QcPKVO.x9RqjrYK.D5Ek7WMatwF0ATlNvwpk9Rs6rvxe8BO', 'ACTIVE');
+	insert into account(id, login, password, money, mmr, status) values
+		(admin_id, 'admin', '$2a$04$fiLDu.QcPKVO.x9RqjrYK.D5Ek7WMatwF0ATlNvwpk9Rs6rvxe8BO', 100, 100, 'ACTIVE');
 
 	insert into account_roles(account_id, role_id) values
 		(admin_id, admin_role_id);
