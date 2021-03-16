e27f8301c135c82c57a84c9bb12f7b13b97dec73
v.krasnov <v.krasnov@lunawealth.com>
2021-03-16 18:45:47 +0300

    add all enums for dictionary

--- src/main/java/org/hse/robowar/controller/DictionaryController.java
+++ src/main/java/org/hse/robowar/controller/DictionaryController.java
@@ -1,6 +1,7 @@
 package org.hse.robowar.controller;
 
-import org.hse.robowar.enums.detail.Armor;
+import org.hse.robowar.enums.Status;
+import org.hse.robowar.enums.detail.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,4 +16,28 @@ public class DictionaryController {
         return ResponseEntity.ok(Armor.values());
     }
 
+    @GetMapping("/basis-type")
+    public ResponseEntity<BasisType[]> getBasisType() {
+        return ResponseEntity.ok(BasisType.values());
+    }
+
+    @GetMapping("/combat")
+    public ResponseEntity<Combat[]> getCombat() {
+        return ResponseEntity.ok(Combat.values());
+    }
+
+    @GetMapping("/movement")
+    public ResponseEntity<Movement[]> getMovement() {
+        return ResponseEntity.ok(Movement.values());
+    }
+
+    @GetMapping("/robot-status")
+    public ResponseEntity<RobotStatus[]> getRobotStatus() {
+        return ResponseEntity.ok(RobotStatus.values());
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Status[]> getStatus() {
+        return ResponseEntity.ok(Status.values());
+    }
 }
