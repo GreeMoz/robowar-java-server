25b32cac49df64ce93b1e24aef83a70bd75639b2
v.krasnov <v.krasnov@lunawealth.com>
2021-03-16 22:41:07 +0300

    add repository scan

--- src/main/java/org/hse/robowar/RobowarApplication.java
+++ src/main/java/org/hse/robowar/RobowarApplication.java
@@ -3,9 +3,11 @@ package org.hse.robowar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("org.hse.robowar.model")
 @SpringBootApplication
+@EnableJpaRepositories("org.hse.robowar.repository")
 public class RobowarApplication {
     public static void main(String[] args) {
         SpringApplication.run(RobowarApplication.class, args);
