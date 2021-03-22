07b575b85d68a27071afd8a4c813c7734fba9d1f
v.krasnov <v.krasnov@lunawealth.com>
2021-03-22 14:54:57 +0300

    minor fix

--- src/main/java/org/hse/robowar/controller/RobotController.java
+++ src/main/java/org/hse/robowar/controller/RobotController.java
@@ -1,40 +1,26 @@
 package org.hse.robowar.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.hse.robowar.dto.AccountDTO;
 import org.hse.robowar.dto.RobotDTO;
-import org.hse.robowar.dto.mapper.AccountMapper;
 import org.hse.robowar.dto.mapper.RobotMapper;
-import org.hse.robowar.model.Robot;
 import org.hse.robowar.service.RobotService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/robot")
 public class RobotController {
-    private final RobotMapper robotMapper;
-    private RobotService robotService;
 
-    @Autowired
-    public RobotController(RobotService robotService, RobotMapper robotMapper) {
-        this.robotService = robotService;
-        this.robotMapper = robotMapper;
-    }
+    private final RobotMapper robotMapper;
+    private final RobotService robotService;
 
     @GetMapping
-    public List<RobotDTO> findAll() {
-        List<Robot> robots = robotService.findAll();
-        List<RobotDTO> robotDTOS = new ArrayList<>();
-        for(int i = 0; i<robots.size(); i++){
-            robotDTOS.add(robotMapper.toDto(robots.get(i)));
-        }
-        return robotDTOS;
+    public ResponseEntity<List<RobotDTO>> findAll() {
+        return ResponseEntity.ok(robotMapper.toDto(robotService.findAll()));
     }
 }
