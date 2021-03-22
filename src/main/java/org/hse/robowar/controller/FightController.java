0b06fb22f7865d51a7a89b114ec836787ec3e003
v.krasnov <v.krasnov@lunawealth.com>
2021-03-22 15:11:27 +0300

    minor fix

--- src/main/java/org/hse/robowar/controller/FightController.java
+++ src/main/java/org/hse/robowar/controller/FightController.java
@@ -3,8 +3,8 @@ package org.hse.robowar.controller;
 import lombok.RequiredArgsConstructor;
 import org.hse.robowar.dto.FightRequestDTO;
 import org.hse.robowar.service.FightService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class FightController {
 
     private final FightService fightService;
 
-    @PostMapping("/fightWithInLeague/{leagueId}/withBot/{botId}")
+    @GetMapping("/fightWithInLeague/{leagueId}/withBot/{botId}")
     public FightRequestDTO fight(@PathVariable("leagueId") UUID leagueId, @PathVariable("botId") UUID botId) throws Exception {
         return fightService.fightInLeague(leagueId, botId);
     }
