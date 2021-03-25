b87b584c3f6d738f2bf7282b7002d898a42f9462
v.krasnov <v.krasnov@lunawealth.com>
2021-03-25 22:55:01 +0300

    insert robot for current

--- src/main/java/org/hse/robowar/controller/RobotController.java
+++ src/main/java/org/hse/robowar/controller/RobotController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.hse.robowar.dto.RobotDTO;
 import org.hse.robowar.dto.RobotWithLeagueDTO;
 import org.hse.robowar.dto.mapper.RobotMapper;
+import org.hse.robowar.model.Robot;
 import org.hse.robowar.repository.RobotRepository;
 import org.hse.robowar.service.AccountService;
 import org.hse.robowar.service.RobotService;
@@ -31,6 +32,14 @@ public class RobotController {
     @PostMapping
     public ResponseEntity<?> insert(@RequestBody RobotDTO robotDTO) {
         robotRepository.save(robotMapper.toEntity(robotDTO));
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("insert-for-current")
+    public ResponseEntity<?> insertForCurrent(@RequestBody RobotDTO robotDTO) {
+        Robot robot = robotMapper.toEntity(robotDTO);
+        robot.setAccount(accountService.getByAuth());
+        robotRepository.save(robot);
         return ResponseEntity.ok().build();
     }
 
